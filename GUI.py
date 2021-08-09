cc6e1672b143c00605cff7b7dd720a97f5cbdc69
DESKTOP-7NVHOOI\Jo <jkyu6554@kpu.ac.kr>
2021-08-10 03:25:38 +0900

    라인 색상지정

--- GUI.py
+++ GUI.py
@@ -11,20 +11,29 @@ def Lines():
     Player_Name_LineX = 80
     Week_Score_LineX =270
     Week_Score_LineX2 = 300
+    Sharenian_Score_LineX = 325
+    Sharenian_Score_LineX2 = 375
+    Flag_Score_LineX = 400
     # 플레이어 이름
-    canvas.create_line(Player_Name_LineX, 0, Player_Name_LineX, 500, fill="black")
+    canvas.create_line(Player_Name_LineX, 0, Player_Name_LineX, 500, fill="#8A7FFF")
 
     # 주간미션
-    canvas.create_line(Week_Score_LineX, 0, Week_Score_LineX, 500, fill="black")
-    canvas.create_line(Week_Score_LineX2, 0, Week_Score_LineX2, 500, fill="black")
+    canvas.create_line(Week_Score_LineX, 0, Week_Score_LineX, 500, fill="#FFFF7F")
+    canvas.create_line(Week_Score_LineX2, 0, Week_Score_LineX2, 500, fill="#FFFF7F")
 
+    #지하수로
+    canvas.create_line(Sharenian_Score_LineX, 0, Sharenian_Score_LineX, 500, fill="#FF7FBF")
+    canvas.create_line(Sharenian_Score_LineX2, 0, Sharenian_Score_LineX2, 500, fill="#FF7FBF")
+
+    #플래그
+    canvas.create_line(Flag_Score_LineX, 0, Flag_Score_LineX, 500, fill="#9F7FFF")
 
 canvas.pack(padx=10,pady=5)
-canvas.create_rectangle(0, 0, 500, 450, fill="red")
+canvas.create_rectangle(0, 0, 500, 450, fill="#590000")
 
 Lines()
 
-window.attributes("-transparentcolor", "#FF0000")
+window.attributes("-transparentcolor", "#590000")
 
 
 
